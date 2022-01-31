e9e926a95e9932adaa2434568edc81d4485196d1
alligator-hub <weballigator2021.03.14@gmail.com>
2022-01-31 10:50:59 +0300

    first commit

---
+++ src/main/java/app/chat/entity/user/Username.java
@@ -0,0 +1,34 @@
+package app.chat.entity.user;
+
+import app.chat.entity.template.AbsMain;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "usernames")
+public class Username extends AbsMain {
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    public Username(String name) {
+        this.name = name;
+    }
+//
+//    @OneToOne(mappedBy = "username")
+//    private User user;
+//
+//    @OneToOne(mappedBy = "username")
+//    private Group group;
+//
+//    @OneToOne(mappedBy = "username")
+//    private Channel channel;
+}
---
+++ src/test/java/app/chat/appchat/Main.java
@@ -0,0 +1,17 @@
+package app.chat.appchat;
+
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+public class Main {
+    String uploadPath = "src/main/resources/files";
+    String uploadPathImage = "src/main/resources/files/image";
+    String uploadPathDocument = "src/main/resources/files/document";
+    String uploadPathVideo = "src/main/resources/files/video";
+    String uploadPathMusic = "src/main/resources/files/music";
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
